601ffca3e8cb5fdb0882a13f0914140a03920f8f
Jeff Wong <v-jwong1@expedia.com>
2020-12-04 21:07:50 +1000

    create entry point index.jsx and App.jsx file

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  // const [cars, setCars] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("/a")
+  //     .then(res => res.json())
+  //     .then(data => setCars(data))
+  //     .catch(err => console.log(`Something went wrong! ${err}`))
+  // }, []);
+
+  // const showNodeVersion = () => {
+  //   console.log(process.env.NODE_VERSION);
+  // }
+
+  // const showAbc = () => {
+  //   console.log(process.env.ABC);
+  // }
+
+  return (
+    <div>
+      <h2>Hello world!</h2>
+    </div>
+  );
+};
+
+export default App;
